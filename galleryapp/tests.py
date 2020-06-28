0edc24a48a9333d99f6f872d52909f4abbbadf66
paulmburu08 <paulmburu08@gmail.com>
2020-06-29 00:34:49 +0300

    add tests

--- galleryapp/tests.py
+++ galleryapp/tests.py
@@ -2,3 +2,11 @@ from django.test import TestCase
 from . models import Location,Category,Image
 
 # Create your tests here.
+class TestLocation(TestCase):
+
+    def setUp(self):
+        self.Ian = Location(location = 'Nairobi')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Ian,Location))
