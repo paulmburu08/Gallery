b25b4ea584a060bdf3d54974537e6609d72e170f
paulmburu08 <paulmburu08@gmail.com>
2020-06-28 18:23:09 +0300

    add save method to Location and Category

--- galleryapp/models.py
+++ galleryapp/models.py
@@ -6,12 +6,18 @@ class Location(models.Model):
 
     def __str__(self):
         return self.location
+    
+    def save_location(self):
+        self.save()
 
 class Category(models.Model):
     category =  models.CharField(max_length=30)
 
     def __str__(self):
         return self.category
+
+    def save_category(self):
+        self.save()
 
 class Image(models.Model):
     image = models.ImageField(upload_to='images/')
@@ -36,4 +42,4 @@ class Image(models.Model):
     def update_image(self):
         self.update()
 
-        
+
