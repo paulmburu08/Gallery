972a2cd0db9604af47bc4feadd8d3d4ae6d61a3d
paulmburu08 <paulmburu08@gmail.com>
2020-06-29 00:37:35 +0300

    add tests

--- galleryapp/tests.py
+++ galleryapp/tests.py
@@ -10,3 +10,9 @@ class TestLocation(TestCase):
     # Testing  instance
     def test_instance(self):
         self.assertTrue(isinstance(self.Ian,Location))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.Ian.save_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) > 0)
