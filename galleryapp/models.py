916a82029d02e7e40539f2939645aa55473a07f8
paulmburu08 <paulmburu08@gmail.com>
2020-06-28 15:55:28 +0300

    add image model

--- galleryapp/models.py
+++ galleryapp/models.py
@@ -7,5 +7,10 @@ class Location(models.Model):
 class Category(models.Model):
     category =  models.CharField(max_length=30)
 
-# class Image(models.Model):
-#     ima
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    upload_date = models.DateTimeField(auto_now_add=True)
+    location = models.ForeignKey(Location, on_delete=models.DO_NOTHING)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
