c74ffcc0572b71bc13c1cb12904102c83260c596
paulmburu08 <paulmburu08@gmail.com>
2020-06-28 15:42:23 +0300

    add location model

--- galleryapp/models.py
+++ galleryapp/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Location(models.Model):
     location = models.CharField(max_length=30)
+
+# class Category(models.Model):
+#     category =  
