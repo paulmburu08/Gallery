2140ede3ee47ae845834a6f40457931c647aeb14
paulmburu08 <paulmburu08@gmail.com>
2020-06-29 03:46:31 +0300

    add filter by location and get image ny id

--- galleryapp/models.py
+++ galleryapp/models.py
@@ -47,8 +47,18 @@ class Image(models.Model):
         self.delete()
 
     @classmethod
+    def get_image_by_id(cls,id):
+        images = cls.objects.filter(id = id)
+        return images
+
+    @classmethod
     def search_by_category(cls,search_term):
         images = cls.objects.filter(category__category__icontains=search_term)
+        return images
+    
+    @classmethod
+    def filter_by_location(cls, location):
+        images = cls.objects.filter(location__location = location)
         return images
 
 
