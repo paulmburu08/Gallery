1126e428ffbbcd14d218e497456ca4bee3f3d521
paulmburu08 <paulmburu08@gmail.com>
2020-06-29 03:11:39 +0300

    add search_results view

--- galleryapp/views.py
+++ galleryapp/views.py
@@ -7,3 +7,16 @@ def landing_page(request):
     title = 'Photospace'
 
     return render(request, 'index.html', {'title':title})
+
+def search_results(request):
+
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html',{"message":message})
